9300f493378afcbc3d8a8df9ea680ca12cd641c6
Asim-Tahir <gmmmasterrpro@gmail.com>
2020-01-20 00:06:38 +0300

    Vuex files added

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+const {state} = require('./state');
+const {mutations} = require('./mutations');
+const {actions} = require('./actions');
+const {modules} = require('./modules');
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+	mutations,	
+  actions,
+  modules
 })
